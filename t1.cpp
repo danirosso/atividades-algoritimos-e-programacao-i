034c8c9eb6f29daa4b582a0c594d956400121841
Luiz <jxssxp@gmail.com>
2026-03-29 17:23:03 -0300

    fix ident

--- t1.cpp
+++ t1.cpp
@@ -1,62 +1,72 @@
 /**************************************************************************************
  * *********************Verificador de CPF TOTAL-LEGIT!********************************
  * Criadores: Alison Brito * Daniel Rosso * Gabriel Alves * Luiz Carlos * Romulo Cesar*
- * **********************            VERSAO 2.0       ********************************
+ * ********************** VERSAO 2.0       ********************************
 **************************************************************************************/
 
 #include <iostream>
-int main (){
-	using namespace std;
-	int i, i1, i2, i3, i4, i5, i6 ,i7, i8, i9, primeiroDigito, segundoDigito;
-	cout << "Insira um numero de oito ou nove digitos: ";
-	cin >> i;
 
-	/********************************************************************************
-	*  podemos também tirar %10 do i e incrimentar este número no i1, i2, i3 etc..  *
-	*  e por cada etapa remover estes numeros do i.                                 *
-	********************************************************************************/
+int main() {
+    using namespace std;
+    int i, i1, i2, i3, i4, i5, i6, i7, i8, i9, primeiroDigito, segundoDigito;
 
-	if (i > 999999 && i <= 999999999){ 
-	i1 = i / 100000000;
-	i = i % 100000000;
-	i2 = i / 10000000;
-	i = i % 10000000;
-	i3 = i / 1000000;
-	i = i % 1000000;
- 	i4 = i / 100000;
-	i = i % 100000;
-	i5 = i / 10000;
-	i = i % 10000;
-	i6 = i / 1000;
-	i = i % 1000;
-	i7 = i / 100;
-	i = i % 100;
-	i8 = i / 10;
-	i = i % 10;
-	i9 = i / 1;
-	i = i % 1;
+    cout << "Insira um numero de oito ou nove digitos: ";
+    cin >> i;
 
-     primeiroDigito = i1 * 10 + i2 * 9 + i3 * 8 + i4 * 7 + i5 * 6 + i6 * 5 + i7 * 4 + i8 * 3 + i9 * 2;
- 	cout<<"Primeira equacao: " << primeiroDigito << "\n";
-	if (primeiroDigito % 11 <  2){
-	     primeiroDigito = 0;
-     }else{
-     primeiroDigito = 11 - (primeiroDigito % 11);
-     };
-     segundoDigito =  i1 * 11 + i2 * 10 + i3 * 9 + i4 * 8 + i5 * 7 + i6 * 6 + i7 * 5 + i8 * 4 + i9 * 3 + primeiroDigito * 2;
-     	cout << "Segunda equacao: " << segundoDigito << "\n";
-	if (segundoDigito % 11 < 2){
-	     segundoDigito = 0;
-     }else{
-     segundoDigito = 11 - (segundoDigito % 11);
-     };     	
-	cout << "O CPF obtido com esses numeros e: " << i1 << i2 << i3 << "." << i4  << i5  << i6 << "."  << i7  << i8 << i9 << "-" << primeiroDigito << segundoDigito << "\n" ;
-	return 0;
-	}else if ( i < 999999 ) {
-		cout << "Voce inseriu um numero menor, reinicie o programa.\n";
-		return 0;
-	}else { 
-		cout << "Voce inseriu um numero maior, reinicie o programa.\n";
-		return 0;
-	};
+    /********************************************************************************
+    * podemos também tirar %10 do i e incrementar este número no i1, i2, i3 etc..  *
+    * e por cada etapa remover estes numeros do i.                                 *
+    ********************************************************************************/
+
+    if (i > 999999 && i <= 999999999) {
+        i1 = i / 100000000;
+        i = i % 100000000;
+        i2 = i / 10000000;
+        i = i % 10000000;
+        i3 = i / 1000000;
+        i = i % 1000000;
+        i4 = i / 100000;
+        i = i % 100000;
+        i5 = i / 10000;
+        i = i % 10000;
+        i6 = i / 1000;
+        i = i % 1000;
+        i7 = i / 100;
+        i = i % 100;
+        i8 = i / 10;
+        i = i % 10;
+        i9 = i / 1;
+        i = i % 1;
+
+        primeiroDigito = i1 * 10 + i2 * 9 + i3 * 8 + i4 * 7 + i5 * 6 + i6 * 5 + i7 * 4 + i8 * 3 + i9 * 2;
+        cout << "Primeira equacao: " << primeiroDigito << "\n";
+
+        if (primeiroDigito % 11 < 2) {
+            primeiroDigito = 0;
+        } else {
+            primeiroDigito = 11 - (primeiroDigito % 11);
+        }
+
+        segundoDigito = i1 * 11 + i2 * 10 + i3 * 9 + i4 * 8 + i5 * 7 + i6 * 6 + i7 * 5 + i8 * 4 + i9 * 3 + primeiroDigito * 2;
+        cout << "Segunda equacao: " << segundoDigito << "\n";
+
+        if (segundoDigito % 11 < 2) {
+            segundoDigito = 0;
+        } else {
+            segundoDigito = 11 - (segundoDigito % 11);
+        }
+
+        cout << "O CPF obtido com esses numeros e: " << i1 << i2 << i3 << "." 
+             << i4 << i5 << i6 << "." << i7 << i8 << i9 << "-" 
+             << primeiroDigito << segundoDigito << "\n";
+        
+        return 0;
+
+    } else if (i < 999999) {
+        cout << "Voce inseriu um numero menor, reinicie o programa.\n";
+        return 0;
+    } else {
+        cout << "Voce inseriu um numero maior, reinicie o programa.\n";
+        return 0;
+    }
 }
